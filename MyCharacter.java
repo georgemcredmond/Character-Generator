557e7cec1632c760b5f87d7b2996b1da978c8da2
George McRedmond <georgem.int101@gmail.com>
2017-07-07 11:10:49 -0400

    Add files via upload
    Run TwoCharacters

---
+++ MyCharacter.java
@@ -0,0 +1,36 @@
+public class MyCharacter
+{
+	private int lives;
+	private int numberOfEyes;
+	private String color;
+	
+	public void setLives(int lvs)
+	{
+		lives = lvs;
+	}
+	
+	public void setNumberOfEyes(int eyes)
+	{
+		numberOfEyes = eyes;
+	}
+	
+	public void setColor(String col)
+	{
+		color = col;
+	}
+	
+	public int getLives()
+	{
+		return lives;
+	}
+	
+	public int getNumberOfEyes()
+	{
+		return numberOfEyes;
+	}
+	
+	public String getColor()
+	{
+		return color;
+	}
+}
